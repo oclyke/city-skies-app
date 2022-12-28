894e3c2236ffa4d77a7e3155c65e1b1c05881838
Owen <o@oclyke.dev>
2022-12-28 13:21:53 -0700

    add city-skies proof of concept

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {
+  useState,
+} from 'react';
+
+import * as expoStatusBar from 'expo-status-bar';
+
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 
 export default function App() {
+  const default_ipaddr = '192.168.0.22';
+  // const default_ipaddr = '';
+  const [ipaddr, setIpAddr] = useState(default_ipaddr);
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <expoStatusBar.StatusBar style="auto" />
+
+      <TextInput
+        value={ipaddr}
+        onChangeText={(value) => setIpAddr(value)}
+        placeholder={'ip address'}
+      />
+
+      <Button
+        title='test city-skies dynamic import'
+        onPress={() => TestCitySkiesEndpoint(`${ipaddr}:1337`)}
+      >
+        
+      </Button>
+
     </View>
   );
 }
@@ -18,3 +48,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function TestCitySkiesEndpoint (host) {
+  fetch(`http://${host}/upload`, {
+    method: 'POST',
+    headers: {
+      // Accept: 'application/json',
+      'Content-Type': 'text/plain',
+    },
+    body: 'demo_dynamic_module'
+  });
+}
