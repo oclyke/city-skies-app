46ee77f75b177733c679939451757047af0a873f
Owen <o@oclyke.dev>
2023-06-09 11:27:50 -0700

    support invalidation in cache

--- src/lib/cache.js
+++ src/lib/cache.js
@@ -1,6 +1,21 @@
 export default class KVStore {
   constructor() {
     this.items = {};
+    this.validations = {};
+  }
+
+  /**
+   * Synchronous method for validating a key
+   */
+  validate(key) {
+    this.validations[key] = true;
+  }
+
+  /**
+   * Synchronous method for invalidating a key
+   */
+  invalidate(key) {
+    this.validations[key] = false;
   }
 
   /**
@@ -9,45 +24,51 @@ export default class KVStore {
    * @param {*} key used to get data from cache items.
    */
   extract(key) {
-    const item = this.items[key];
-    if (typeof item === 'undefined') {
-      throw new Error('cache miss');
+    const valid = this.validations[key];
+    if (typeof valid === 'undefined') {
+      throw new Error('cache miss [nonexistent]');
     }
+    if (valid !== true) {
+      throw new Error('cache miss [invalid]');
+    }
+    const data = this.items[key];
+    return data;
   }
 
   /**
    * Synchronous method for storing value by key.
-   * @param {*} key 
-   * @param {*} value 
+   * @param {*} key
+   * @param {*} value
    */
   store(key, value) {
     this.items[key] = value;
+    this.validate(key);
   }
 
   /**
    * Async method for getting value by key.
-   * @param {*} key 
-   * @returns 
+   * @param {*} key
+   * @returns
    */
   get(key) {
     return new Promise((resolve, reject) => {
-      const item = this.items[key];
-      if (typeof item === 'undefined') {
-        reject(new Error('cache miss'));
-      } else {
-        resolve(item);
+      try {
+        const data = this.extract(key);
+        resolve(data);
+      } catch (e) {
+        reject(e);
       }
     });
   }
 
   /**
    * Async method for storing value by key.
-   * @param {*} key 
-   * @param {*} value 
-   * @returns 
+   * @param {*} key
+   * @param {*} value
+   * @returns
    */
   put(key, value) {
-    this.items[key] = value;
+    this.store(key, value);
     return Promise.resolve();
   }
 
